41b14a612ad7c3e510c55bb32e7e36e0c234e20c
shivam8801 <shivammh15@gmail.com>
2023-10-26 15:19:45 +0530

    added logic to clear jwt token

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createUser, loginUser, checkAuth } from "../controller/Auth.js";
+import { createUser, loginUser, checkAuth, logout } from "../controller/Auth.js";
 import passport from "passport";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router
   .post("/signup", createUser)
   .post("/login", passport.authenticate("local"), loginUser)
-  .get("/check", passport.authenticate("jwt"), checkAuth);
+  .get("/check", passport.authenticate("jwt"), checkAuth)
+  .get("/logout", logout);
 
 export default router;
